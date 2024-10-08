ba7ee547b45fea7b94933c3d028a89e8317a1742
Ana Reyna <anareymej@gmail.com>
2024-10-08 17:35:02 +0200

    add matchers for private and public pages

---
+++ src/app/(private)/events/page.tsx
@@ -0,0 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
+
+export default function EventsPage() {
+	return (
+		<>
+			<h1>Events</h1>
+			<UserButton />
+		</>
+	);
+}
